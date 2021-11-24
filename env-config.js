b9110edba542295431221caaedfa250605fd76e4
ArunG <arung.sap@gmail.com>
2021-11-24 23:33:55 +0530

    Fix broken link to single post

--- env-config.js
+++ env-config.js
@@ -2,4 +2,5 @@ const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
   'process.env.NEXT_PUBLIC_STRAPI_API_URL': prod ? 'https://lmnas-cms.herokuapp.com' : 'http://localhost:1337',
+  'process.env.BACKEND_URL': prod ? '/lmnas-cms-blog' : '', 
 }
