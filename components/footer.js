6874f7427ef13aa8162a83cd40cdec4d95ca2e9c
srinath1204 <srinathmuniyasamy1204@gmail.com>
2022-04-21 19:33:21 +0530

    draft version of blog

--- components/footer.js
+++ components/footer.js
@@ -52,16 +52,18 @@ export default function Footer() {
                 </div>
                 <li>
                   <div className='flex justify-start'>
+                  <a href="tel:98840 44470">
                     <svg xmlns="http://www.w3.org/2000/svg" className="md:h-6 md:w-6 h-4 w-4 inline text-th-accent-medium " viewBox="0 0 20 20" fill="currentColor">
                       <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                     </svg>
                     <span className='text-sm md:text-lg ml-2'>
                       +91 98840 44470
-                    </span>
+                    </span></a>
                   </div>
                 </li>
                 <div className='flex justify-start'>
                   <li>
+                  <a href="hello@lmnas.com">
                     <svg xmlns="http://www.w3.org/2000/svg" className="md:h-6 md:w-6 h-4 w-4 inline text-th-accent-medium " viewBox="0 0 20 20" fill="currentColor">
                       <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                       <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
@@ -69,6 +71,7 @@ export default function Footer() {
                     <span className='text-sm md:text-lg ml-2'>
                     hello@lmnas.com
                     </span>
+                    </a>
                   </li>
                 </div>
               </li>
