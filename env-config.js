e0e663810ac5e708e0f61ba88a27e93873398f62
Fathima786Irfana <fathima.irfana@lmnas.com>
2024-10-11 09:28:32 +0530

    Changed the HOME_SITE_URL value

--- env-config.js
+++ env-config.js
@@ -8,7 +8,8 @@ module.exports = {
    // 'process.env.NEXT_PUBLIC_STRAPI_API_URL': prod ? 'http://localhost:1337' : 'http://localhost:1337',
    'process.env.NEXT_PUBLIC_STRAPI_API_URL': prod ? 'https://dumps-240005.lmnaslens.com' : 'https://dumps-240005.lmnaslens.com',
    'process.env.STRAPI_PREVIEW_SECRET':'LMNAS',
-   'process.env.HOME_SITE_URL':"http://localhost:3000",
+   //'process.env.HOME_SITE_URL':"http://localhost:3000",
+   'process.env.HOME_SITE_URL':"https://lmnas.docker.localhost",
    'process.env.URL':"https://lenslms.docker.localhost",
    "process.env.AUTH_BASE_64":"Basic ODliOTA5ZDExYWIyYWVjOmU5NGFkMzc2N2ZkNjYzOQ=="
 }
